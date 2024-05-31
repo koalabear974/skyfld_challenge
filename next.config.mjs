12bc02a85b034692be08bd1cb1f0fc6cf0bd8043
Adrien Robert <a.robert@rt-iut.re>
2024-05-31 09:08:16 +0200

    Try vercel fix for file database inclusion

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/sensors/data': ['./database/**/*'],
+    },
+  },
+};
 
 export default nextConfig;
